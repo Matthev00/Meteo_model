c2862da594905b8212707067b3cf3eeb54a1e67c
misadowsk <michalsad.protondynamic@gmail.com>
2024-11-16 12:18:25 +0100

    feat: added simple objective funciton that optimize hyperparameters with optuna
    Refers: #19

--- meteo_model/training/perform_experiments.py
+++ meteo_model/training/perform_experiments.py
@@ -4,6 +4,8 @@ import os
 import torch
 import itertools
 from torch.utils.data import DataLoader, TensorDataset
+# from meteo_model.data.data_loader import create_dataloaders
+import optuna
 
 
 class Model(torch.nn.Module):
@@ -13,7 +15,8 @@ class Model(torch.nn.Module):
 
     def forward(self, x):
         return self.linear(x)
-    
+
+
 def create_dataloaders(input_len, output_len, batch_size, location, split_ratio, num_workers):
     num_samples = 100 
     inputs = torch.randn(num_samples, input_len)
@@ -25,45 +28,86 @@ def create_dataloaders(input_len, output_len, batch_size, location, split_ratio,
     return dataloader, dataloader
 
 
+def objective(trial):
+    input_len = trial.suggest_int("input_len", 1, 100)
+    output_len = 8
+    batch_size = trial.suggest_int("batch_size", 1, 100)
+    lr = trial.suggest_float("lr", 1e-5, 1e-1)
+    epochs = trial.suggest_int("epochs", 1, 10)
+
+    train_loader, test_loader = create_dataloaders(
+        location=["BIALYSTOK", "WARSAW", "WROCLAW", "KRAKOW", "POZNAN"],
+        input_len=input_len,
+        output_len=output_len,
+        split_ratio=0.8,
+        batch_size=batch_size,
+        num_workers=os.cpu_count(),
+    )
+
+    model = Model(input_size=input_len, output_size=output_len)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.MSELoss()
+
+    results = train(
+        model=model,
+        train_dataloader=train_loader,
+        test_dataloader=test_loader,
+        optimizer=optimizer,
+        loss_fn=loss_fn,
+        epochs=epochs,
+        device="cpu",
+        enable_logging=True,
+    )
+
+    return results["Test_MSE"][-1]
+
+
 def main():
-    NUM_WORKERS = os.cpu_count()
-    OUTPUT_LEN = 8
-    SPLIT_RATIO = 0.8
-    DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # NUM_WORKERS = os.cpu_count()
+    # OUTPUT_LEN = 8
+    # SPLIT_RATIO = 0.8
+    # DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    params = {
-        "location": [["BIALYSTOK", "WARSAW", "WROCLAW", "KRAKOW", "POZNAN"], ["BIALYSTOK"]],
-        "input_len": [32],
-        "batch_size": [16],
-        "lr": [0.001],
-        "epochs": [5],
-    }
+    # params = {
+    #     "location": [["BIALYSTOK", "WARSAW", "WROCLAW", "KRAKOW", "POZNAN"], ["BIALYSTOK"]],
+    #     "input_len": [32],
+    #     "batch_size": [16],
+    #     "lr": [0.001],
+    #     "epochs": [5],
+    # }
 
-    keys, values = zip(*params.items())
-    combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
+    # keys, values = zip(*params.items())
+    # combinations = [dict(zip(keys, v)) for v in itertools.product(*values)]
 
-    for combo in combinations:
-        model = Model(input_size=combo["input_len"], output_size=OUTPUT_LEN).to(DEVICE)
-        OPTIMIZER = torch.optim.Adam(model.parameters(), lr=combo["lr"])
-        train_loader, test_loader = create_dataloaders(
-            location=combo["location"],
-            input_len=combo["input_len"],
-            output_len=OUTPUT_LEN,
-            split_ratio=SPLIT_RATIO,
-            batch_size=combo["batch_size"],
-            num_workers=NUM_WORKERS,
-        )
+    # for combo in combinations:
+    #     model = Model(input_size=combo["input_len"], output_size=OUTPUT_LEN).to(DEVICE)
+    #     OPTIMIZER = torch.optim.Adam(model.parameters(), lr=combo["lr"])
+    #     train_loader, test_loader = create_dataloaders(
+    #         location=combo["location"],
+    #         input_len=combo["input_len"],
+    #         output_len=OUTPUT_LEN,
+    #         split_ratio=SPLIT_RATIO,
+    #         batch_size=combo["batch_size"],
+    #         num_workers=NUM_WORKERS,
+    #     )
 
-        results = train(
-            model=model,
-            train_dataloader=train_loader,
-            test_dataloader=test_loader,
-            optimizer=OPTIMIZER,
-            loss_fn=torch.nn.MSELoss(),
-            epochs=combo["epochs"],
-            device=DEVICE,
-            enable_logging=False,
-        )
+    #     results = train(
+    #         model=model,
+    #         train_dataloader=train_loader,
+    #         test_dataloader=test_loader,
+    #         optimizer=OPTIMIZER,
+    #         loss_fn=torch.nn.MSELoss(),
+    #         epochs=combo["epochs"],
+    #         device=DEVICE,
+    #         enable_logging=True,
+    #     )
+
+    study = optuna.create_study(direction="minimize")
+    study.optimize(objective, n_trials=10)
+
+    print("Best trial:")
+    print(f"  Value: {study.best_trial.value}")
+    print(f"  Params: {study.best_trial.params}")
 
 
 if __name__ == "__main__":
