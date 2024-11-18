271b910ec8d1abd9163afcfd31accf5ee12fd5e0
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-11-18 12:21:05 +0100

    feat: Added inverse of normalization. Snow normalised using log of mean

--- meteo_model/data/normaliser.py
+++ meteo_model/data/normaliser.py
@@ -36,7 +36,7 @@ def _normalise_prcp(array):
 def _normalise_snow(array):
     mean_snow = stats["snow"]["mean"]
     mean_snow_log = np.log1p(mean_snow)
-    return np.log1p(array) / mean_snow
+    return np.log1p(array) / mean_snow_log
 
 def _normalise_wdir(array):
     rad_array = np.deg2rad(array)
@@ -48,9 +48,46 @@ def _normalise_wspd(array):
 def _normalise_pres(array):
     return _normalise_norm_like(array, "pres")
 
+def _inverse_normalize_norm_like(array, attr_name):
+    mean = stats[attr_name]["mean"]
+    std = stats[attr_name]["std"]
+    return array * std + mean
+
+def _inverse_pres(array):
+    return _inverse_normalize_norm_like(array, 'pres')
+
+def _inverse_tavg(array):
+    return _inverse_normalize_norm_like(array, 'tavg')
+
+def _inverse_tmin(array):
+    return _inverse_normalize_norm_like(array, 'tmin')
+
+def _inverse_tmax(array):
+    return _inverse_normalize_norm_like(array, 'tmax')
+
+def _inverse_wspd(array):
+    return _inverse_normalize_norm_like(array, 'wspd')
+
+def _inverse_prcp(array):
+    return np.expm1(array)
+
+def _inverse_snow(array):
+    mean_snow = stats["snow"]["mean"]
+    mean_snow_log = np.log1p(mean_snow)
+    return np.expm1(array* mean_snow_log)
+
+def _inverse_wdir(array_sin, array_cos):
+    angle_rad = np.arctan2(array_sin, array_cos)
+    angle_deg = np.rad2deg(angle_rad)
+    if angle_deg < 0:
+        angle_deg += 360
+    return angle_deg
+
+
+
+
 def normalize_data(df):
-    columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'sin_wdir', 'cos_wdir', 'wspd', 'wpgt', 'pres',
-       'tsun']
+    columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'sin_wdir', 'cos_wdir', 'wspd', 'pres']
     normalised_df = pd.DataFrame(columns=columns)
     normalised_df["tavg"] = _normalise_tavg(df["tavg"])
     normalised_df["tmin"] = _normalise_tmin(df["tmin"])
@@ -63,6 +100,20 @@ def normalize_data(df):
     normalised_df["wspd"] = _normalise_wspd(df["wspd"])
     normalised_df["pres"] = _normalise_pres(df["pres"])
     return normalised_df
+
+
+def inverse_normalize_data(df):
+    columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'wdir', 'wspd', 'pres']
+    inv_df = pd.DataFrame(columns=columns)
+    inv_df['tavg'] = _inverse_tavg(df['tavg'])
+    inv_df['tmin'] = _inverse_tmin(df['tmin'])
+    inv_df['tmax'] = _inverse_tmax(df['tmax'])
+    inv_df['prcp'] = _inverse_prcp(df['prcp'])
+    inv_df['snow'] = _inverse_snow(df['snow'])
+    inv_df['wdir'] = _inverse_wdir(df['sin_wdir'], df['cos_wdir'])
+    inv_df['pres'] = _inverse_pres(df['pres'])
+    return inv_df
+
 
 
 
