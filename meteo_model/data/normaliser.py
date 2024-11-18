eae9ab58fd6b4981cf4f08cddbe72de32c47436c
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-11-18 14:05:07 +0100

    chore: Iterating over all norm like columns onstead of calling dedicated functions

--- meteo_model/data/normaliser.py
+++ meteo_model/data/normaliser.py
@@ -20,16 +20,6 @@ def _normalise_norm_like(array, attr_name: str):
     return (array - mean)/std
 
 
-
-def _normalise_tavg(array):
-    return _normalise_norm_like(array, "tavg")
-
-def _normalise_tmin(array):
-    return _normalise_norm_like(array, "tmin")
-
-def _normalise_tmax(array):
-    return _normalise_norm_like(array, "tmax")
-
 def _normalise_prcp(array):
     return np.log1p(array)
 
@@ -42,31 +32,11 @@ def _normalise_wdir(array):
     rad_array = np.deg2rad(array)
     return np.sin(rad_array), np.cos(rad_array)
 
-def _normalise_wspd(array):
-    return _normalise_norm_like(array, "wspd")
-
-def _normalise_pres(array):
-    return _normalise_norm_like(array, "pres")
 
 def _inverse_normalize_norm_like(array, attr_name):
     mean = stats[attr_name]["mean"]
     std = stats[attr_name]["std"]
     return array * std + mean
-
-def _inverse_pres(array):
-    return _inverse_normalize_norm_like(array, 'pres')
-
-def _inverse_tavg(array):
-    return _inverse_normalize_norm_like(array, 'tavg')
-
-def _inverse_tmin(array):
-    return _inverse_normalize_norm_like(array, 'tmin')
-
-def _inverse_tmax(array):
-    return _inverse_normalize_norm_like(array, 'tmax')
-
-def _inverse_wspd(array):
-    return _inverse_normalize_norm_like(array, 'wspd')
 
 def _inverse_prcp(array):
     return np.expm1(array)
@@ -88,30 +58,26 @@ def _inverse_wdir(array_sin, array_cos):
 
 def normalize_data(df):
     columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'sin_wdir', 'cos_wdir', 'wspd', 'pres']
+    norm_like_columns = ['tavg', 'tmin', 'tmax', 'wspd', 'pres']
     normalised_df = pd.DataFrame(columns=columns)
-    normalised_df["tavg"] = _normalise_tavg(df["tavg"])
-    normalised_df["tmin"] = _normalise_tmin(df["tmin"])
-    normalised_df["tmax"] = _normalise_tmax(df["tmax"])
+    for col in norm_like_columns:
+        normalised_df[col] = _normalise_norm_like(df[col], col)
+
     normalised_df["prcp"] = _normalise_prcp(df["prcp"])
     normalised_df["snow"] = _normalise_snow(df["snow"])
-    sin_wdir, cos_wdir = _normalise_wdir(df["wdir"])
-    normalised_df["sin_wdir"] = sin_wdir
-    normalised_df["cos_wdir"] = cos_wdir
-    normalised_df["wspd"] = _normalise_wspd(df["wspd"])
-    normalised_df["pres"] = _normalise_pres(df["pres"])
+    normalised_df["sin_wdir"], normalised_df["cos_wdir"] = _normalise_wdir(df["wdir"])
     return normalised_df
 
 
 def inverse_normalize_data(df):
     columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'wdir', 'wspd', 'pres']
     inv_df = pd.DataFrame(columns=columns)
-    inv_df['tavg'] = _inverse_tavg(df['tavg'])
-    inv_df['tmin'] = _inverse_tmin(df['tmin'])
-    inv_df['tmax'] = _inverse_tmax(df['tmax'])
+    norm_like_columns = ['tavg', 'tmin', 'tmax', 'wspd', 'pres']
+    for col in norm_like_columns:
+        inv_df[col] = _inverse_normalize_norm_like(df[col], col)
     inv_df['prcp'] = _inverse_prcp(df['prcp'])
     inv_df['snow'] = _inverse_snow(df['snow'])
     inv_df['wdir'] = _inverse_wdir(df['sin_wdir'], df['cos_wdir'])
-    inv_df['pres'] = _inverse_pres(df['pres'])
     return inv_df
 
 
