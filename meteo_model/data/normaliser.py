516d6a41ccad13441b683a58dda995a9f1721445
Szymon Łukawski <122651215+Szymens@users.noreply.github.com>
2024-11-18 14:11:36 +0100

    chore: removed global variable, removed literal from plt.save_fig

--- meteo_model/data/normaliser.py
+++ meteo_model/data/normaliser.py
@@ -7,14 +7,8 @@ from get_stats import create_stat_file
 import pandas as pd
 
 
-if not Path(PATH_TO_STATS).exists():
-    create_stat_file()
 
-with open(PATH_TO_STATS) as f:
-    stats = json.load(f)
-
-
-def _normalise_norm_like(array, attr_name: str):
+def _normalise_norm_like(array, attr_name: str, stats):
     mean = stats[attr_name]["mean"]
     std = stats[attr_name]["std"]
     return (array - mean)/std
@@ -23,7 +17,7 @@ def _normalise_norm_like(array, attr_name: str):
 def _normalise_prcp(array):
     return np.log1p(array)
 
-def _normalise_snow(array):
+def _normalise_snow(array, stats):
     mean_snow = stats["snow"]["mean"]
     mean_snow_log = np.log1p(mean_snow)
     return np.log1p(array) / mean_snow_log
@@ -33,7 +27,7 @@ def _normalise_wdir(array):
     return np.sin(rad_array), np.cos(rad_array)
 
 
-def _inverse_normalize_norm_like(array, attr_name):
+def _inverse_normalize_norm_like(array, attr_name, stats):
     mean = stats[attr_name]["mean"]
     std = stats[attr_name]["std"]
     return array * std + mean
@@ -41,7 +35,7 @@ def _inverse_normalize_norm_like(array, attr_name):
 def _inverse_prcp(array):
     return np.expm1(array)
 
-def _inverse_snow(array):
+def _inverse_snow(array, stats):
     mean_snow = stats["snow"]["mean"]
     mean_snow_log = np.log1p(mean_snow)
     return np.expm1(array* mean_snow_log)
@@ -56,27 +50,27 @@ def _inverse_wdir(array_sin, array_cos):
 
 
 
-def normalize_data(df):
+def normalize_data(df, stats):
     columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'sin_wdir', 'cos_wdir', 'wspd', 'pres']
     norm_like_columns = ['tavg', 'tmin', 'tmax', 'wspd', 'pres']
     normalised_df = pd.DataFrame(columns=columns)
     for col in norm_like_columns:
-        normalised_df[col] = _normalise_norm_like(df[col], col)
+        normalised_df[col] = _normalise_norm_like(df[col], col, stats)
 
     normalised_df["prcp"] = _normalise_prcp(df["prcp"])
-    normalised_df["snow"] = _normalise_snow(df["snow"])
+    normalised_df["snow"] = _normalise_snow(df["snow"], stats)
     normalised_df["sin_wdir"], normalised_df["cos_wdir"] = _normalise_wdir(df["wdir"])
     return normalised_df
 
 
-def inverse_normalize_data(df):
+def inverse_normalize_data(df, stats):
     columns = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'wdir', 'wspd', 'pres']
     inv_df = pd.DataFrame(columns=columns)
     norm_like_columns = ['tavg', 'tmin', 'tmax', 'wspd', 'pres']
     for col in norm_like_columns:
-        inv_df[col] = _inverse_normalize_norm_like(df[col], col)
+        inv_df[col] = _inverse_normalize_norm_like(df[col], col, stats)
     inv_df['prcp'] = _inverse_prcp(df['prcp'])
-    inv_df['snow'] = _inverse_snow(df['snow'])
+    inv_df['snow'] = _inverse_snow(df['snow'], stats)
     inv_df['wdir'] = _inverse_wdir(df['sin_wdir'], df['cos_wdir'])
     return inv_df
 
@@ -88,10 +82,20 @@ if __name__== "__main__":
     import matplotlib.pyplot as plt
     from glob import glob
 
+    PATH_TO_NORMALISED_HISTOGRAMS = "reports/figures/data/data_featues_histograms_normalised.png"
+
+
+    if not Path(PATH_TO_STATS).exists():
+        create_stat_file()
+
+    with open(PATH_TO_STATS) as f:
+        stats_ = json.load(f)
+
+
     variables = ['tavg', 'tmin', 'tmax', 'prcp', 'snow', 'sin_wdir', 'cos_wdir', 'wspd', 'pres']
     plt.figure(figsize=(12, 10))
     huge_df = get_dataframe(glob(PATHS_TO_DATA_FILES_STR))
-    normalised = normalize_data(huge_df)
+    normalised = normalize_data(huge_df, stats_)
 
     for i, var in enumerate(variables, 1):
         plt.subplot(5, 2, i)  # 5 rows, 2 columns grid
@@ -101,5 +105,5 @@ if __name__== "__main__":
         plt.ylabel('Frequency')
 
     plt.tight_layout()
-    plt.savefig("reports/figures/data/data_featues_histograms_normalised.png")
+    plt.savefig(PATH_TO_NORMALISED_HISTOGRAMS)
     plt.show()
