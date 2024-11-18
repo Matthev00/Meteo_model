f55f6dab5003c8f17fd0ac70d9c5329de7d07ad4
Matthev00 <mateuszostaszewskififa@gmail.com>
2024-11-18 22:18:21 +0100

    feat: update BaseWeatherModel constructor to include output length and improve parameter naming

--- meteo_model/model/base_model.py
+++ meteo_model/model/base_model.py
@@ -4,11 +4,17 @@ import torch
 
 
 class BaseWeatherModel(ABC, nn.Module):
-    def __init__(self, input_size, output_size, num_locations):
+    def __init__(self, num_features, num_locations: int, output_len: int):
+        """
+        Args:
+            num_features (int): The number of features in the input data.
+            num_locations (int): The number of locations in the input data.
+            output_len (int): The number of days to predict.
+        """
         super(BaseWeatherModel, self).__init__()
-        self.input_size = input_size
-        self.output_size = output_size
+        self.num_features = num_features
         self.num_locations = num_locations
+        self.output_len = output_len
 
     @abstractmethod
     def forward(self, x: torch.Tensor) -> torch.Tensor:
