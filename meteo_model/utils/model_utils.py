a392fd2002121e9a2f427ca9dea5fedad1d27242
Matthev00 <mateuszostaszewskififa@gmail.com>
2024-11-27 17:01:16 +0100

    feat: implement model loading utility using MLflow

---
+++ meteo_model/utils/model_utils.py
@@ -0,0 +1,6 @@
+import mlflow.pytorch
+from meteo_model.model.base_model import BaseWeatherModel
+
+
+def load_model(model_name: str, model_version: int) -> BaseWeatherModel:
+    return mlflow.pytorch.load_model(model_uri=f"models:/{model_name}/{model_version}")
