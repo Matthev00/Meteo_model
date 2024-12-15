10f24dff79a693ea6ec4d4f97b83da087f0f00c0
Matthev00 <mateuszostaszewskififa@gmail.com>
2024-12-15 13:53:38 +0100

    refactor: remove move_models_to_project_dir function and associated code

--- meteo_model/utils/mlflow_utils.py
+++ meteo_model/utils/mlflow_utils.py
@@ -23,27 +23,3 @@ def update_meta_yaml(mlruns_path: str, old_prefix: str, new_prefix: str) -> None
 
                         with open(file_path, "w") as f:
                             yaml.safe_dump(meta_data, f)
-
-
-def move_models_to_project_dir():
-    model_for_days = {
-        1: ("MeteoModel-1_day", 2),
-        2: ("MeteoModel-2_days", 1),
-        3: ("MeteoModel-3_days", 1),
-        4: ("MeteoModel-4_days", 1),
-        5: ("MeteoModel-5_days", 1),
-        6: ("MeteoModel-6_days", 2),
-        7: ("MeteoModel-7_days", 2),
-        8: ("MeteoModel-8_days", 2),
-    }
-
-    for n_days, (model_name, v) in model_for_days.items():
-        model = load_model(model_name, v)
-        model_path = "models/" + model_name
-        os.makedirs("models", exist_ok=True)
-        torch.save(model.state_dict(), model_path + ".pth")
-    
-
-if __name__ == "__main__":
-
-    move_models_to_project_dir()
